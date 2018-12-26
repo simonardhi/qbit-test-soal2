c195aac6017fa45a87b91d5f43c251c35aeba793
sayindo <sayindo@sayindo.sayindo.com>
2018-12-26 10:35:06 +0700

    Fixing mocha test

--- test/appTest.js
+++ test/appTest.js
@@ -3,13 +3,14 @@ const input = require('../server/app').lexicograph;
 
 
 describe('Lexigrophic()', function () {
-    it('soal2 should be return string', function(){
-        assert.equal(input('hahahhah'), 'ah')
-    })
-    it('soal2 should be return string', function(){
-        assert.equal(input('breabes'), 'saerb')
-    })
-    it('soal2 should be return string', function(){
-        assert.equal(input('arebaes'), 'saber')
-    })
+	var tests = [
+	    {args: "hahahhah", expected: "ah"},
+	    {args: "breabes",  expected: "saerb"},
+	    {args: "arebaes",  expected: "saber"}
+	  ];
+    tests.forEach(function(test) {
+    	it('correctly answer ' + test.args.length + ' args', function() {
+    		assert.equal(tests.args, test.expected);
+	    });
+	});	
 });
